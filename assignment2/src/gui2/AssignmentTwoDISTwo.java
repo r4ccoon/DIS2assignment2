db2d162dbe07c16dc664ac39b565995278274d4a
ridho <rpl.oye@gmail.com>
2014-04-25 20:52:48 +0200

    add rohan code

---
+++ assignment2/src/gui2/AssignmentTwoDISTwo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignmenttwodistwo;
+
+/**
+ *
+ * @author rohan
+ */
+public class AssignmentTwoDISTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        WindowSystem orgasm ;
+        orgasm = new WindowSystem( 500   , 500 );
+        
+        
+        
+    }
+    
+}
---
+++ assignment2/src/gui2/WindowSystem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignmenttwodistwo;
+import  de.rwth.hci.Graphics.* ;
+
+/**
+ *
+ * @author rohan
+ */
+public class WindowSystem extends GraphicsEventSystem {
+
+    public WindowSystem(int width, int height) {
+        super(width, height);
+    }
+
+    protected void handlePaint() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+
+}
